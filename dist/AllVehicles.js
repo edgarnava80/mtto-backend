f26c1ff396f27b988042fc5088bfc7ee8f2af26f
edgarnava80 <edgarnr@hotmail.com>
2021-01-04 12:07:20 -0600

    Morgan middleware added

--- dist/AllVehicles.js
+++ dist/AllVehicles.js
@@ -7,7 +7,8 @@ const vehicles = [
         model: "Versa",
         estimatedate: "2021/12/01",
         id: 3340,
-        image: "http://3.23.108.188/cars/jetta.jpg"
+        //image: "http://3.23.108.188/cars/jetta.jpg"
+        image: "https://file.kelleybluebookimages.com/kbb/base/evox/CP/13142/2019-Nissan-Versa-front_13142_032_2400x1800_QM1.png"
     },
     {
         description: " motor adjustment",
@@ -15,7 +16,8 @@ const vehicles = [
         model: "CR-V",
         estimatedate: "",
         id: 3341,
-        image: "http://3.23.108.188/cars/CR-V.jpg"
+        //image: "http://3.23.108.188/cars/CR-V.jpg"
+        image: "https://static.tcimg.net/vehicles/primary/9c8c9300d83bbc9e/2020-Honda-CR-V-white-full_color-driver_side_front_quarter.png"
     },
     {
         description: " engine tuning ",
@@ -24,7 +26,8 @@ const vehicles = [
         estimatedate: "2020/20/12",
         id: 3342,
         km: 90000,
-        image: "http://3.23.108.188/cars/civic.jpg"
+        //image: "http://3.23.108.188/cars/civic.jpg"
+        image: "https://file.kelleybluebookimages.com/kbb/base/evox/CP/8792/2013-Honda-Civic-front_8792_032_2400x1800_WA.png"
     },
     {
         description: " oil change ",
@@ -32,7 +35,8 @@ const vehicles = [
         model: "Tiguan",
         km: 13500,
         id: 3343,
-        image: "http://3.23.108.188/cars/tiguan.jpg"
+        //image: "http://3.23.108.188/cars/tiguan.jpg"
+        image: "https://file.kelleybluebookimages.com/kbb/base/evox/CP/8792/2013-Honda-Civic-front_8792_032_2400x1800_WA.png"
     },
     {
         description: " change of pads ",
@@ -40,7 +44,8 @@ const vehicles = [
         model: "Sentra",
         km: 6000,
         id: 3344,
-        image: "http://3.23.108.188/cars/sentra.jpg"
+        //image: "http://3.23.108.188/cars/sentra.jpg"
+        image: "https://s.aolcdn.com/commerce/autodata/images/USC90NIC061C021001.jpg"
     },
     {
         description: " change of pads ",
@@ -56,7 +61,8 @@ const vehicles = [
         model: "Aveo NG",
         estimatedate: "2021/09/07",
         km: 33460,
-        image: "http://3.23.108.188/cars/aveo.jpg"
+        //image: "http://3.23.108.188/cars/aveo.jpg"
+        image: "https://www.motortrend.com/uploads/sites/10/2015/11/2011-chevrolet-aveo-1ls-sd-sedan-angular-front.png"
     },
     {
         description: "Change ligths",
--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
-//import morgan from "morgan"
+const morgan_1 = __importDefault(require("morgan"));
 const vehicleRoutes_1 = __importDefault(require("./routes/vehicleRoutes"));
 const app = express_1.default();
-//app.use(morgan("dev"))
+if (process.env.NODE_ENV === "development")
+    app.use(morgan_1.default("dev"));
 app.use(body_parser_1.default.json());
 app.use(express_1.default.static(`${__dirname}/public`));
 app.use((req, res, next) => {
